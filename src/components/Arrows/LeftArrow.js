2485c3bd5cb7639357739ddcee4e25cb245780b6
unknown <workdennis@yahoo.com>
2018-06-22 08:47:57 -0500

    committing to gh-pages

---
+++ src/components/Arrows/LeftArrow.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./LeftArrow.css";
+
+const LeftArrow = () => (
+  <div id = "left-container">
+  	<span id ="left"> <a href = "#"> &lArr; </a> </span> 
+  </div>
+
+);
+
+export default LeftArrow;
